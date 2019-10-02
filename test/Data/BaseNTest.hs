199932969dabd701b6566a8192569af367f38f65
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2019-10-02 07:23:33 -0500

    Improvement

--- test/Data/BaseNTest.hs
+++ test/Data/BaseNTest.hs
@@ -17,18 +17,18 @@ spec_examples :: Spec
 spec_examples = do
   describe "Edge Cases" $ do
     it "Empty" $ do
-      encodeBase2 ([] :: [Word8]) `shouldBe` ""
-      encodeBase8 ([] :: [Word8]) `shouldBe` ""
-      decodeBase2 "" `shouldBe` Right ([] :: [Word8])
-      decodeBase8 "" `shouldBe` Right ([] :: [Word8])
+      goodEnc encodeBase2 [] ""
+      goodEnc encodeBase8 [] ""
+      goodDec decodeBase2 "" []
+      goodDec decodeBase8 "" []
     it "Malformed Base2" $ do
-      decodeBase2 "0" `shouldBe` (Left (WrongLength 7) :: Result)
-      decodeBase2 "000011110000" `shouldBe`(Left (WrongLength 4) :: Result)
-      decodeBase2 "01234567" `shouldBe` (Left UnknownAlphabet :: Result)
+      badDec decodeBase2 "0" $ WrongLength 7
+      badDec decodeBase2 "000011110000" $ WrongLength 4
+      badDec decodeBase2 "01234567" UnknownAlphabet
     it "Malformed Base8" $ do
-      decodeBase8 "0" `shouldBe` (Left (WrongLength 2) :: Result)
-      decodeBase8 "0123456701234" `shouldBe` (Left (WrongLength 1) :: Result)
-      decodeBase8 "01234567890" `shouldBe` (Left UnknownAlphabet :: Result)
+      badDec decodeBase8 "0" $ WrongLength 2
+      badDec decodeBase8 "0123456701234" $ WrongLength 1
+      badDec decodeBase8 "01234567890" UnknownAlphabet
   describe "Base2 Spec" $ do
     testEnc2 [0] "00000000"
     testEnc2 [1] "00000001"
@@ -43,7 +43,11 @@ spec_examples = do
     testEnc8 [123] "366"
     testEnc8 [0,0] "000000"
     testEnc8 [213, 231] "653634"
+    testEnc8 [0, 10, 20, 30, 40, 50] "0000502407424062"
   where
+    goodEnc c b t   = c (b :: [Word8]) `shouldBe` t
+    goodDec c t b   = c t `shouldBe` (Right b :: Result)
+    badDec  c t err = c t `shouldBe` (Left err :: Result)
     testEnc2 = testEncGen encodeBase2
     testEnc8 = testEncGen encodeBase8
     testEncGen enc i s = it (show i) $ enc (i :: [Word8]) `shouldBe` s
